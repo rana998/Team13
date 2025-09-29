92dfccb101ea590e3ce18d80c6d35249d268d967
RaghadAljuid <s220003258@seu.edu.sa>
2025-09-29 11:43:59 +0300

    تعديل رغد

---
+++ Team13/Raghad.swift
@@ -0,0 +1,18 @@
+//
+//  Raghad.swift
+//  Team13
+//
+//  Created by Raghad Aljuid on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct Raghad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Raghad()
+}
