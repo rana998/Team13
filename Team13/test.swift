6c706dbeb6b90a6b02b941ca13c8927e3fdcbf44
RaghadAljuid <s220003258@seu.edu.sa>
2025-09-30 10:49:03 +0300

    add file

---
+++ Team13/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  Team13
+//
+//  Created by Raghad Aljuid on 08/04/1447 AH.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
