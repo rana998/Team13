3ede65f3573be2c2fc40b6e5dae3c788ea9a6cd8
Nouf Alshawoosh <Noufshawoosh@gmail.com>
2025-10-05 15:13:53 +0300

    Update

--- Team13/Board_Screen.swift
+++ Team13/Board_Screen.swift
@@ -28,11 +28,11 @@ struct Board_Screen: View {
             
             VStack(spacing: 0){
                 HStack(alignment: .top, spacing: 22){
-                Text("My Board")
+                    Text("My Board")
                         .font(.system(size: 24))
                         .foregroundStyle(.white)
                         .bold()
-                       
+                    
                     
                     Button(action: {
                         currentTool = currentTool == .nailRemover ? .none : .nailRemover
@@ -63,76 +63,77 @@ struct Board_Screen: View {
                     .padding(.trailing, 10)
                 
                 
-                    
-                    Rectangle()
-                        .fill(Color.clear)
-                        .frame(height: 650)
-                        .contentShape(Rectangle())
-                        .offset(y: -45)
-                        .gesture(
-                            DragGesture(minimumDistance: 0)
-                                .onEnded { value in
-                                    let location = value.location
-                                    // print("Tapped at: \(location)")
-                                    switch currentTool {
-                                    case .nail:
-                                        nailPositions.append(location)
+                
+                Rectangle()
+                    //.stroke(Color.black)
+                    .fill(Color.clear)
+                    .frame(height: 650)
+                    .contentShape(Rectangle())
+                    .offset(y: -85)
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onEnded { value in
+                                let location = value.location
+                                // print("Tapped at: \(location)")
+                                switch currentTool {
+                                case .nail:
+                                    nailPositions.append(location)
+                                    
+                                case .nailRemover:
+                                    if let index = nailPositions.firstIndex(where: {
+                                        nail in
+                                        let distance = sqrt(pow(nail.x - location.x, 2) + pow(nail.y - location.y, 2))
+                                        return distance < 50
+                                    }){
+                                        let removedNail = nailPositions[index]
+                                        nailPositions.remove(at: index)
+                                        removedNailPositions.append(removedNail)
                                         
-                                    case .nailRemover:
-                                        if let index = nailPositions.firstIndex(where: {
-                                            nail in
-                                            let distance = sqrt(pow(nail.x - location.x, 2) + pow(nail.y - location.y, 2))
-                                                    return distance < 50
-                                        }){
-                                            let removedNail = nailPositions[index]
-                                            nailPositions.remove(at: index)
-                                            removedNailPositions.append(removedNail)
-                                            
-                                        }
-                                        
-                                    case .none:
-                                        break
                                     }
-                                }
-                        )
-                        .overlay(
-                            ZStack {
-                                // display all nails
-                                ForEach(nailPositions.indices, id: \.self) { index in
-                                    Image("Nail")
-                                        .position(nailPositions[index])
-                                        .onTapGesture {
-                                            if currentTool == .nailRemover {
-                                                let removedNail = nailPositions[index]
-                                                removedNailPositions.append(removedNail)
-                                                nailPositions.remove(at: index)
-                                                }
-                                            }
-                                }
-                                
-                                // display all removed nail spots
-                                ForEach(removedNailPositions.indices, id: \.self) { index in
-                                    Image("RemovedNailSpot")
-                                        .position(removedNailPositions[index])
+                                    
+                                case .none:
+                                    break
                                 }
                             }
-                        )
-                
-                
-                
-                }
-                
-
-                
-            }
+                    )
+                    .overlay(
+                        ZStack {
+                            // display all nails
+                            ForEach(nailPositions.indices, id: \.self) { index in
+                                Image("Nail")
+                                    .position(nailPositions[index])
+                                    .onTapGesture {
+                                        if currentTool == .nailRemover {
+                                            let removedNail = nailPositions[index]
+                                            removedNailPositions.append(removedNail)
+                                            nailPositions.remove(at: index)
+                                        }
+                                    }
+                            }
+                            
+                            // display all removed nail spots
+                            ForEach(removedNailPositions.indices, id: \.self) { index in
+                                Image("RemovedNailSpot")
+                                    .position(removedNailPositions[index])
+                            }
+                        }
+                    )
                 
                 
                 
             }
             
-        }
+            
+            
+        }.ignoresSafeArea(.container, edges: .bottom)
         
+        
+        
+    }
     
+}
+
+
 
 
 #Preview {
